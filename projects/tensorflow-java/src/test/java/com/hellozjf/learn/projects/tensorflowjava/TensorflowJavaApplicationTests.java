93e8a1e597b9346d3d02d516629fee8f2f3895c1
hellozjf <908686171@qq.com>
2019-08-16 16:09:37 +0800

    【提交模块】tensorflow-java 【提交目的】使用java代码直接调用pb模型

---
+++ projects/tensorflow-java/src/test/java/com/hellozjf/learn/projects/tensorflowjava/TensorflowJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hellozjf.learn.projects.tensorflowjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TensorflowJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
