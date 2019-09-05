12a98f39e4faa7e2e263ae749cd62f79dd3d418b
hellozjf <908686171@qq.com>
2019-09-05 09:37:54 +0800

    【提交模块】order12306 【提交目的】增加aop

---
+++ springboot2/src/main/java/com/hellozjf/learn/springboot2/aop/ControllerAspect.java
@@ -0,0 +1,29 @@
+package com.hellozjf.learn.springboot2.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jingfeng Zhou
+ */
+@Component
+@Aspect
+@Slf4j
+public class ControllerAspect {
+
+    @Around("execution(* com.hellozjf.learn.springboot2.controller.TestController.*(..))")
+    public Object testControllerCost(ProceedingJoinPoint invocation) throws Throwable {
+        return calcTime(invocation);
+    }
+
+    private Object calcTime(ProceedingJoinPoint invocation) throws Throwable {
+        long t1 = System.currentTimeMillis();
+        Object result = invocation.proceed();
+        long t2 = System.currentTimeMillis();
+        log.debug("{} cost {}", invocation.getSignature(), t2 - t1);
+        return result;
+    }
+}
